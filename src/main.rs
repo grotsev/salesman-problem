b12acede1e08709b24e6378251bf9bb9e67673a0
den <denis.grotsev@gmail.com>
2019-11-14 11:19:08 +0300

    Edge condense initial solution

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate image;
 extern crate imageproc;
 
 use rand::prelude::*;
-use std::time::Instant;
+use std::time::{Instant};
 use std::collections::BTreeSet;
 use image::{Rgb, RgbImage};
 use imageproc::drawing::{draw_antialiased_line_segment_mut, draw_filled_rect_mut};
@@ -18,16 +18,17 @@ fn main() {
     let start = Instant::now();
     let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
 
-    let size: P = 1000;
+    let size: P = 100;
     let index = |a: P, b: P| a as usize * size as usize + b as usize;
-    let points: Vec<Coord> = (0..size).map(|_| (rng.gen_range(0, 1000), rng.gen_range(0, 1000))).collect();
+    let width: u32 = 256;
+    let points: Vec<Coord> = (0..size).map(|_| (rng.gen_range(0, width as i32), rng.gen_range(0, width as i32))).collect();
 
     let cost_matrix: Vec<u32> = {
         let mut r = Vec::with_capacity(size as usize * size as usize);
         for a in 0..size {
             for b in 0..size {
-                r.push(((points[a as usize].0 - points[b as usize].0) as f64)
-                    .hypot((points[a as usize].1 - points[b as usize].1) as f64)
+                r.push((((points[a as usize].0 - points[b as usize].0) as f64)
+                    .hypot((points[a as usize].1 - points[b as usize].1) as f64) * 100f64)
                     .ceil() as u32)
             }
         }
@@ -36,7 +37,7 @@ fn main() {
 
     let cost = |a: P, b: P| cost_matrix[index(a, b)];
 
-    let near_matrix: Vec<P> = {
+    /*let near_matrix: Vec<P> = {
         let mut r = Vec::with_capacity(size as usize * (size as usize - 1));
         for a in 0..size {
             r.extend(0..a);
@@ -48,36 +49,69 @@ fn main() {
     };
 
     let near = |a: P| &near_matrix[a as usize * (size as usize - 1)..(a as usize + 1) * (size as usize - 1)];
-
-    let draw = |visits: &Vec<P>| {
-        let mut image: RgbImage = RgbImage::new(1000, 1000);
+*/
+    let draw = |visits: &Vec<P>, i:u32| {
+        let mut image: RgbImage = RgbImage::new(width, width);
         let black = Rgb([0u8, 0u8, 0u8]);
+        let gray = Rgb([200u8, 200u8, 200u8]);
         let white = Rgb([255u8, 255u8, 255u8]);
         let yellow = Rgb([255u8, 255u8, 0u8]);
 
-        draw_filled_rect_mut(&mut image, Rect::at(0, 0).of_size(1000, 1000), white);
+        draw_filled_rect_mut(&mut image, Rect::at(0, 0).of_size(width, width), white);
 
-        fn m((x, y): (i32, i32)) -> (i32, i32) { (x + 1, y + 2) }
+        for p in &points {
+            draw_filled_rect_mut(&mut image, Rect::at(p.0-1, p.1-1).of_size(3, 3), black);
+        }
+        /*fn m((x, y): (i32, i32)) -> (i32, i32) { (x + 1, y + 2) }
         for i in 0..size {
             let a = m(points[i as usize]);
             let b = m(points[near(i)[0] as usize]);
             draw_antialiased_line_segment_mut(&mut image, a, b, yellow, interpolate);
-        }
+        }*/
 
         let coord = |i| {
             points[visits[i as usize] as usize]
         };
         let mut prev = coord(0);
-        for i in 1..=size {
+        for i in 1..visits.len() {
             let curr = coord(i);
-            draw_antialiased_line_segment_mut(&mut image, prev, curr, black, interpolate);
+            draw_antialiased_line_segment_mut(&mut image, prev, curr, gray, interpolate);
             prev = curr;
         }
 
-        image.save("plot.png").unwrap();
+        image.save(format!("plot/{}.png", i)).unwrap();
     };
 
-    let mut best = { // greedy
+    let pot = |a: P, b: P, c: P| cost(a, c) + cost(c, b) - cost(a, b);
+
+    let mut best = Vec::with_capacity(size as usize);
+    best.push(0);
+    best.push(0);
+    let mut best_cost: u32 = 0;
+    let mut rest: BTreeSet<P> = (1..size).collect();
+    let mut i = 0;
+    while !&rest.is_empty() {
+        let mut min_pot = std::u32::MAX;
+        let mut min_b = std::usize::MAX;
+        let mut min_c = std::u16::MAX;
+        for b in 1..best.len() {
+            for c in &rest {
+                let p = pot(best[b - 1], best[b], *c);
+                if p < min_pot {
+                    min_pot = p;
+                    min_b = b;
+                    min_c = *c;
+                }
+            }
+        }
+        best.insert(min_b, min_c);
+        rest.remove(&min_c);
+        best_cost += min_pot;
+        draw(&best, i);
+        i+=1;
+    }
+
+    /*let mut best = { // greedy
         let mut r: Vec<P> = Vec::with_capacity(size as usize);
         let mut prev = 0;
         r.push(prev);
@@ -112,8 +146,8 @@ fn main() {
             best[a as usize..=b as usize].reverse();
             best_cost = best_cost - e.0 + e.1;
         }
-    }
+    }*/
     println!("{:?} {:?}", start.elapsed(), best_cost);
-    draw(&best);
+    //draw(&best);
 }
 
