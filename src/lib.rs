899f368be212ade49d4aa251d6dff11c8a09d45b
den <denis.grotsev@gmail.com>
2019-11-22 14:46:40 +0300

    Cycle heuristics

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,52 @@
 /*use priority_queue::PriorityQueue;
-use std::cmp::Ordering;
+use std::cmp::{Ordering, Reverse};
 use std::collections::HashSet;
 
 type P = usize;
 
 type PP = (P, P);
 
-struct Merge {
-    up: bool,
-    cost: u32,
-    sub: [PP; 2],
-    add: [PP; 2],
-}
-
-impl Ord for Merge {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.up.cmp(&other.up.cmp)
-            .then(other.cost.cmp(&self.cost))
-    }
-}
-
-fn sort(a: [P; 2]) -> [P; 2] {
-    if a[0] < a[1] { a } else { [a[1], a[0]] }
+fn ord(a: P, b: P) -> (P, P) {
+    if a < b { (a, b) } else { (b, a) }
 }
 
 fn solve(size: P, cost: fn(P, P) -> u32) -> Vec<([P; 2], Merge)> {
-    let mut merge: Vec<([P; 2], Merge)> = (0..size).map(|a| [
-        ([a, a], Merge { up: false, cost: 0, sub: [(a, a), (a, a)], add: [(a, a), (a, a)] }),
-    ]).collect();
+    let mut cyc: Vec<HashSet<PP>> = (0..size).map(|a| {
+        let mut set = HashSet::new();
+        set.insert((a, a));
+        set
+    }).collect();
 
     let mut actual: HashSet<P> = (0..size).collect();
-    let mut queue: PriorityQueue<[P; 2], Merge> = PriorityQueue::new();
+    let mut queue: PriorityQueue<PP, Reverse<u32>> = PriorityQueue::new();
     for a in 0..size - 1 {
         for b in a + 1..size {
-            queue.push([a, b], Merge { up: false, cost: 2 * cost(a, b), sub: [(a, a), (b, b)], add: [(a, b), (a, b)] });
+            queue.push((a, b), Reverse(2 * cost(a, b)));
         }
     };
+
+    let union = |a: &HashSet<PP>, b: &HashSet<PP>| -> HashSet<PP> {
+
+    }
+
+    let pot = |a: &HashSet<PP>, b: &HashSet<PP>| -> Reverse<u32> {
+
+    }
+
     for m in size..2 * size - 1 {
-        let reify = queue.pop().unwrap();
-        merge.push(reify);
-        actual.remove(&reify.0[0]);
-        actual.remove(&reify.0[1]);
+        let ((a, b), _) = queue.pop().unwrap();
+        cyc.push(union(&cyc[a], &cyc[b]));
+        actual.remove(&a);
+        actual.remove(&b);
         for other in actual {
-            let trash: ([P; 2], Merge) = reify.0.map(|r| {
-                queue.change_priority_by(&sort([r, other]), |mut p| {
-                    p.up = true;
-                    p
-                });
-                queue.pop().unwrap()
-            }).min_by(|t| t.1.cost);
-            queue.push([other, m], Merge {
-                up: false,
-                cost: 0,
-                sub: [],
-                add: [],
-            });
+            queue.change_priority(&ord(a, other), Reverse(0));
+            queue.change_priority(&ord(b, other), Reverse(0));
+            queue.pop().unwrap();
+            queue.pop().unwrap();
+            queue.push((other, m), pot(&cyc[other], &cyc[m]));
         }
         actual.push(m);
     };
     merge
-}
-
-fn main() {
-
 }
 */
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate imageproc;
 
 use rand::prelude::*;
 use std::time::Instant;
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 use image::{Rgb, RgbImage};
 use imageproc::drawing::{draw_antialiased_line_segment_mut, draw_filled_rect_mut};
 use imageproc::rect::Rect;
@@ -13,6 +13,115 @@ use imageproc::pixelops::interpolate;
 
 type Coord = (i32, i32);
 type P = u16;
+type PP = (u16, u16);
+
+#[derive(Debug)]
+struct Clew(Vec<PP>);
+
+impl Clew {
+    fn new(size: P) -> Clew {
+        Clew((0..size).map(|i| (i, i)).collect())
+    }
+
+    fn iter(&self) -> ClewIterator {
+        ClewIterator {
+            clew: &self,
+            current: 0,
+            second: false,
+        }
+    }
+
+    fn yarn(&self, (current, next): PP) -> YarnIterator {
+        let x = self.0[current as usize];
+        assert!(x.0 == next || x.1 == next, "Not adjancent next [{:?}]={:?} {:?}", current, x, next);
+        YarnIterator {
+            clew: &self,
+            start: current,
+            current,
+            next,
+            moved: false,
+        }
+    }
+
+    fn cas(&mut self, pos: P, next: P, new: P) {
+        let x = &mut self.0[pos as usize];
+        if x.0 == next {
+            x.0 = new;
+        } else {
+            assert_eq!(x.1, next);
+            x.1 = new;
+        }
+    }
+
+    fn get_not(&self, pos: P, next: P) -> u16 {
+        let x = self.0[pos as usize];
+        if x.0 == next {
+            x.1
+        } else {
+            assert_eq!(x.1, next);
+            x.0
+        }
+    }
+
+    fn merge(&mut self, (a, b): PP, (c, d): PP) {
+        self.cas(a, b, c);
+        self.cas(b, a, d);
+        self.cas(c, d, a);
+        self.cas(d, c, b);
+    }
+}
+
+struct ClewIterator<'a> {
+    clew: &'a Clew,
+    current: P,
+    second: bool,
+}
+
+impl Iterator for ClewIterator<'_> {
+    type Item = PP;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.current < self.clew.0.len() as P {
+            let i = self.current;
+            let (prev, next) = self.clew.0[i as usize];
+            let v = if self.second {
+                self.current += 1;
+                next
+            } else { prev };
+            self.second = !self.second;
+            if i < v || (self.second && i == v) {
+                return Some((i, v));
+            }
+        }
+        None
+    }
+}
+
+struct YarnIterator<'a> {
+    clew: &'a Clew,
+    start: P,
+    current: P,
+    next: P,
+    moved: bool,
+}
+
+impl Iterator for YarnIterator<'_> {
+    type Item = PP;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start == self.current && self.moved { None } else {
+            self.moved = true;
+            let t = self.current;
+            self.current = self.next;
+            self.next = self.clew.get_not(self.next, t);
+            Some(ord((t, self.current)))
+        }
+    }
+}
+
+fn ord<T: Ord>((a, b): (T, T)) -> (T, T) {
+    if a <= b { (a, b) } else { (b, a) }
+}
 
 fn main() {
     let start = Instant::now();
@@ -23,13 +132,13 @@ fn main() {
     let width: u32 = 256;
     let points: Vec<Coord> = (0..size).map(|_| (rng.gen_range(0, width as i32), rng.gen_range(0, width as i32))).collect();
 
-    let cost_matrix: Vec<u32> = {
+    let cost_matrix: Vec<i32> = {
         let mut r = Vec::with_capacity(size as usize * size as usize);
         for a in 0..size {
             for b in 0..size {
                 r.push((((points[a as usize].0 - points[b as usize].0) as f64)
                     .hypot((points[a as usize].1 - points[b as usize].1) as f64) * 100f64)
-                    .ceil() as u32)
+                    .ceil() as i32)
             }
         }
         r
@@ -37,143 +146,68 @@ fn main() {
 
     let cost = |a: P, b: P| cost_matrix[index(a, b)];
 
-    /*let near_matrix: Vec<P> = {
-        let mut r = Vec::with_capacity(size as usize * (size as usize - 1));
-        for a in 0..size {
-            r.extend(0..a);
-            r.extend(a + 1..size);
-            r[a as usize * (size as usize - 1)..(a as usize + 1) * (size as usize - 1)]
-                .sort_by_key(|&b| cost(a, b));
-        }
-        r
-    };
-
-    let near = |a: P| &near_matrix[a as usize * (size as usize - 1)..(a as usize + 1) * (size as usize - 1)];
-*/
-    let draw = |visits: &Vec<P>, i: u32| {
+    let draw = |clew: &Clew, i: u32| {
         let mut image: RgbImage = RgbImage::new(width, width);
         let black = Rgb([0u8, 0u8, 0u8]);
         let gray = Rgb([200u8, 200u8, 200u8]);
         let white = Rgb([255u8, 255u8, 255u8]);
-        let yellow = Rgb([255u8, 255u8, 0u8]);
+        let _yellow = Rgb([255u8, 255u8, 0u8]);
 
         draw_filled_rect_mut(&mut image, Rect::at(0, 0).of_size(width, width), white);
 
         for p in &points {
             draw_filled_rect_mut(&mut image, Rect::at(p.0 - 1, p.1 - 1).of_size(3, 3), black);
         }
-        /*fn m((x, y): (i32, i32)) -> (i32, i32) { (x + 1, y + 2) }
-        for i in 0..size {
-            let a = m(points[i as usize]);
-            let b = m(points[near(i)[0] as usize]);
-            draw_antialiased_line_segment_mut(&mut image, a, b, yellow, interpolate);
-        }*/
 
-        let coord = |i| {
-            points[visits[i as usize] as usize]
+        let coord = |p| {
+            points[p as usize]
         };
-        let mut prev = coord(0);
-        for i in 1..visits.len() {
-            let curr = coord(i);
-            draw_antialiased_line_segment_mut(&mut image, prev, curr, gray, interpolate);
-            prev = curr;
+        for f in clew.iter() {
+            draw_antialiased_line_segment_mut(&mut image, coord(f.0), coord(f.1), gray, interpolate);
         }
 
         image.save(format!("plot/{}.png", i)).unwrap();
     };
 
-    let pot = |a: P, b: P, c: P| cost(a, c) + cost(c, b) - cost(a, b);
-
-    let mut best = Vec::with_capacity(size as usize);
-    best.push(0);
-    best.push(0);
-    let mut best_cost: u32 = 0;
-    let mut rest: BTreeSet<P> = (1..size).collect();
-    let mut i = 0;
-    while !&rest.is_empty() {
-        let (mut min_b, mut min_c, mut min_p) = (std::usize::MAX, std::u16::MAX, std::u32::MAX);
-        for b in 1..best.len() {
-            for c in &rest {
-                let p = pot(best[b - 1], best[b], *c);
-                if p < min_p {
-                    min_b = b;
-                    min_c = *c;
-                    min_p = p;
-                }
-            }
+    let pot = move |(a, b): PP, (c, d): PP| {
+        let acbd = cost(a, c) + cost(b, d);
+        let adbc = cost(a, d) + cost(b, c);
+        let abcd = cost(a, b) + cost(c, d);
+        if acbd <= adbc {
+            (acbd - abcd, (c, d))
+        } else {
+            (adbc - abcd, (d, c))
         }
-        best.insert(min_b, min_c);
-        rest.remove(&min_c);
-        best_cost += min_p;
-
-        let estimate = |best: &Vec<P>, a: P, b: P| {
-            let az = best[a as usize];
-            let am = best[a as usize - 1];
-            let bz = best[b as usize];
-            let bp = best[b as usize + 1];
-            (cost(am, az) + cost(bz, bp),
-             cost(am, bz) + cost(az, bp))
-        };
-
-        (min_b + 2..best.len() - 1)
-            .map(|b| (b, estimate(&best, min_b as P + 1, b as u16)))
-            .filter(|(b, e)| e.0 > e.1)
-            .max_by_key(|(b, e)| e.0 - e.1)
-            .map(|(b, e)| {
-                best[min_b + 1..=b].reverse();
-                best_cost = best_cost - e.0 + e.1;
-            });
-
-        (1..min_b - 1)
-            .map(|a| (a, estimate(&best, a as u16, min_b as P - 1)))
-            .filter(|(a, e)| e.0 > e.1)
-            .max_by_key(|(a, e)| e.0 - e.1)
-            .map(|(a, e)| {
-                best[a..=min_b - 1].reverse();
-                best_cost = best_cost - e.0 + e.1;
-            });
-
-        draw(&best, i);
-        i += 1;
-    }
-
-    /*let mut best = { // greedy
-        let mut r: Vec<P> = Vec::with_capacity(size as usize);
-        let mut prev = 0;
-        r.push(prev);
-        let mut rest: BTreeSet<P> = (1..size).collect();
-        while !rest.is_empty() {
-            prev = *near(prev).iter().find(|next| rest.contains(next)).unwrap();
-            r.push(prev);
-            rest.remove(&prev);
-        }
-        r.push(0);
-        r
     };
 
-    let mut best_cost =
-        (1..best.len()).map(|i| cost(best[i - 1], best[i])).sum::<u32>();
+    let mut clew = Clew::new(size);
+    let mut cycle: HashSet<PP> = (0..size).map(|a| (a, a)).collect();
 
-    for _ in 0..2000000 {
-        let a = rng.gen_range(1, size - 1);
-        let b = rng.gen_range(a + 1, size);
+    for m in 0..size - 1 {
+        let cl = &clew;
+        let (c1, c2, (a1, _a2, (_p, a2x))) = cycle.iter()
+            .flat_map(|&c1| cycle.iter()
+                .filter(move |&c2| c1 != *c2)
+                .map(move |&c2| {
+                    let a12px = cl.yarn(c1).
+                        flat_map(|a1| cl.yarn(c2).map(move |a2| (a1, a2, pot(a1, a2))))
+                        .min_by_key(|(_a1, _a2, (p, _a2x))| *p)
+                        .unwrap();
+                    (c1, c2, a12px)
+                })
+            )
+            .min_by_key(|(_c1, _c2, (_a1, _a2, (p, _a2x)))| *p)
+            .unwrap();
+        cycle.remove(&c1);
+        cycle.remove(&c2);
+        cycle.insert((a1.0, a2x.0));
+        clew.merge(a1, a2x);
+        //draw(&clew, m as u32);
+    }
 
-        let estimate = |a: P, b: P| {
-            let az = best[a as usize];
-            let am = best[a as usize - 1];
-            let bz = best[b as usize];
-            let bp = best[b as usize + 1];
-            (cost(am, az) + cost(bz, bp),
-             cost(am, bz) + cost(az, bp))
-        };
-        let e = estimate(a, b);
+    let best_cost: i32 = clew.iter().map(|(a, b)| cost(a, b)).sum();
 
-        if e.1 <= e.0 {
-            best[a as usize..=b as usize].reverse();
-            best_cost = best_cost - e.0 + e.1;
-        }
-    }*/
     println!("{:?} {:?}", start.elapsed(), best_cost);
-    //draw(&best);
+    draw(&clew, 999);
 }
 
