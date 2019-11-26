5cf7acf919ffc24a489237f8aa71bb43f9bf25ef
den <denis.grotsev@gmail.com>
2019-11-26 16:56:05 +0300

    Alg

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use imageproc::drawing::{draw_antialiased_line_segment_mut, draw_filled_rect_mut
 use imageproc::rect::Rect;
 use imageproc::pixelops::interpolate;
 use priority_queue::PriorityQueue;
+use crate::Alg::{Rev, RotL, RotR};
 
 type Coord = (i32, i32);
 type P = u16;
@@ -18,6 +19,13 @@ type PP = (u16, u16);
 
 fn ord<T: Ord>((a, b): (T, T)) -> (T, T) {
     if a <= b { (a, b) } else { (b, a) }
+}
+
+#[derive(Debug)]
+enum Alg {
+    Rev,
+    RotL,
+    RotR,
 }
 
 fn main() {
@@ -49,7 +57,7 @@ fn main() {
         draw_filled_rect_mut(&mut image, Rect::at(0, 0).of_size(width, width), white);
         for (i, p) in points.iter().enumerate() {
             draw_filled_rect_mut(&mut image, Rect::at(p.0 - 1, p.1 - 1).of_size(3, 3), black);
-            draw_text_mut(&mut image, black, p.0 as u32 + 2, p.1 as u32, scale, &font, &i.to_string());
+            //draw_text_mut(&mut image, black, p.0 as u32 + 2, p.1 as u32, scale, &font, &i.to_string());
         }
         for i in 1..gofn.len() {
             draw_antialiased_line_segment_mut(&mut image, points[gofn[i - 1] as usize], points[gofn[i] as usize], red, interpolate);
@@ -57,13 +65,13 @@ fn main() {
         image.save(format!("plot/{}.png", i)).unwrap();
     };
 
-    let cost_matrix: Vec<i32> = {
+    let cost_matrix: Vec<u32> = {
         let mut r = Vec::with_capacity(size as usize * size as usize);
         for a in 0..size {
             for b in 0..size {
                 r.push((((points[a as usize].0 - points[b as usize].0) as f64)
                     .hypot((points[a as usize].1 - points[b as usize].1) as f64) * 100f64)
-                    .ceil() as i32)
+                    .ceil() as u32)
             }
         }
         r
@@ -71,90 +79,77 @@ fn main() {
 
     let cost = |a: P, b: P| cost_matrix[index(a, b)];
 
-    let mut nofg: Vec<P> = (0..size).collect();
     let mut gofn: Vec<P> = (0..size).collect();
+    //gofn[1..size as usize - 1].shuffle(&mut rng);
+    //gofn[1..size as usize - 1].shuffle(&mut rng);
 
-    let pot = move |nofg: &Vec<P>, gofn: &Vec<P>, g1, g2| {
-        if g1 == g2 {
-            return None;
-        }
-        let n1 = nofg[g1 as usize];
-        let n2 = nofg[g2 as usize];
-        let (n1, g1, n2, g2) = if n1 < n2 { (n1, g1, n2, g2) } else { (n2, g2, n1, g1) };
-        if n1 == 0 || n2 >= size - 2 {
-            return None;
-        }
-        let n0 = n1 - 1;
-        let n3 = n2 + 1;
-        let g0 = gofn[n0 as usize];
+    let rev = move |gofn: &Vec<P>, n1:P, n3:P| {
+        let g0 = gofn[n1 as usize - 1];
+        let g1 = gofn[n1 as usize];
+        let g2 = gofn[n3 as usize - 1];
         let g3 = gofn[n3 as usize];
 
         let old = cost(g0, g1) + cost(g2, g3);
         let new = cost(g0, g2) + cost(g1, g3);
         if new < old {
-            Some((ord((g1, g2)), (old - new) as u32))
+            Some((Rev, n1, n3, old - new))
         } else {
             None
         }
     };
 
-    let nofgr = &nofg;
-    let gofnr = &gofn;
-    let mut pofg1g2: PriorityQueue<PP, u32> = (1..size - 2)
-        .flat_map(|g1| (g1 + 1..size - 1).filter_map(move |g2| pot(nofgr, gofnr, g1, g2)))
-        .collect();
+    let rotl = move |gofn: &Vec<P>, n1:P, n3:P| {
+        let g0 = gofn[n1 as usize - 1];
+        let g = gofn[n1 as usize];
+        let g1 = gofn[n1 as usize + 1];
+        let g2 = gofn[n3 as usize - 1];
+        let g3 = gofn[n3 as usize];
 
-    //println!("{:?}", pofg1g2);
+        let old = cost(g0, g) + cost(g, g1) + cost(g2, g3);
+        let new = cost(g0, g1) + cost(g2, g) + cost(g, g3);
+        if new < old {
+            Some((RotL, n1, n3, old - new))
+        } else {
+            None
+        }
+    };
+
+    let rotr = move |gofn: &Vec<P>, n1:P, n3:P| {
+        let g0 = gofn[n1 as usize - 1];
+        let g1 = gofn[n1 as usize];
+        let g2 = gofn[n3 as usize - 2];
+        let g = gofn[n3 as usize - 1];
+        let g3 = gofn[n3 as usize];
+
+        let old = cost(g0, g1) + cost(g2, g) + cost(g, g3);
+        let new = cost(g0, g) + cost(g, g1) + cost(g2, g3);
+        if new < old {
+            Some((RotR, n1, n3, old - new))
+        } else {
+            None
+        }
+    };
 
     let mut i = 0;
-    println!("{:?}", pofg1g2);
-    draw(&gofn, i);
-    i += 1;
-    while let Some(((g1, g2), p)) = pofg1g2.pop() {
-        println!("================== {:?}", i);
-        let n1 = nofg[g1 as usize];
-        let n2 = nofg[g2 as usize];
-        let (n1, g1, n2, g2) = if n1 < n2 { (n1, g1, n2, g2) } else { (n2, g2, n1, g1) };
-        let n0 = n1 - 1;
-        let n3 = n2 + 1;
-        let n1_n2 = n1 + n2;
-        let g0 = gofn[n0 as usize];
-        let g3 = gofn[n3 as usize];
-        println!("{:?} {:?} {:?} {:?} {:?}", g0, g1, g2, g3, p);
-        //assert_eq!((ord((g1, g2)), p), pot(g0, g1, g2, g3).unwrap());
-
-        for g in 1..size - 1 {
-            for &gi in &[g0, g1, g2, g3] {
-                pot(&nofg, &gofn, gi, g).map(|((g1, g2), _p)| {
-                    pofg1g2.push((g1, g2), std::u32::MAX);
-                    assert_eq!((g1, g2), *pofg1g2.peek().unwrap().0);
-                    pofg1g2.pop();
-                });
-            }
+    while let Some((alg, n1, n3, p)) = {
+        let gofnr = &gofn;
+        (1..size - 2).flat_map(|n1| (n1 + 2..size).filter_map(move |n3| rev(gofnr, n1, n3)))
+            .chain((1..size - 2).flat_map(|n1| (n1 + 3..size).filter_map(move |n3| rotl(gofnr, n1, n3))))
+            .chain((1..size - 2).flat_map(|n1| (n1 + 3..size).filter_map(move |n3| rotr(gofnr, n1, n3))))
+            .max_by_key(|(_, _, _, p)| *p)
+    } {
+        let r = &mut gofn[n1 as usize..n3 as usize];
+        match alg {
+            Rev => r.reverse(),
+            RotL => r.rotate_left(1),
+            RotR => r.rotate_right(1),
         }
-
-        (n1..n3).for_each(|n| {
-            let g = gofn[n as usize] as usize;
-            assert_eq!(nofg[g], n); // TODO remove
-            nofg[g] = n1_n2 - nofg[g]; // TODO try performance n1_n3 - n
-        });
-        gofn[n1 as usize..n3 as usize].reverse();
-        println!("{:?}", gofn);
-
-        for g in 1..size - 1 {
-            for &gi in &[g0, g1, g2, g3] {
-                pot(&nofg, &gofn, gi, g).map(|((g1, g2), p)| {
-                    pofg1g2.push((g1, g2), p);
-                });
-            }
-        }
-
-        println!("{:?}", pofg1g2);
-        draw(&gofn, i);
+        //draw(&gofn, i);
         i += 1;
     }
 
-    println!("{:?}", start.elapsed());
-    //draw(&gofn, 999);
+    let s: u32 = (1..gofn.len()).map(|i| cost(gofn[i], gofn[i - 1])).sum();
+    println!("{:?} {:?} {:?}", start.elapsed(), i, s);
+    draw(&gofn, i);
 }
 
